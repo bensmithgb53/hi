6b548fb96c86b52e2a72a4eb10117cd83c06df2b
bensmithgb53 <104525191+bensmithgb53@users.noreply.github.com>
2023-03-08 22:38:29 +0000

    Update build.gradle.kts

--- RainoStreamsProvider/build.gradle.kts
+++ RainoStreamsProvider/build.gradle.kts
@@ -21,5 +21,5 @@ cloudstream {
         "Live",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=rainostreams.com&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=methstreams.me/&sz=%size%"
 }
